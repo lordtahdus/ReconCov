5e070ef4c396dd755718786a2c66cb4c1fc2602f
Vincent Su <datsu2003@gmail.com>
2025-05-27 12:19:44 +1000

    fix importing functions for my package

--- R/wrangle_sim_result.R
+++ R/wrangle_sim_result.R
@@ -3,34 +3,30 @@
 #' @param MSE A list of matrices where each matrix corresponds to a model's MSE values.
 #' @param to_ts Logical indicating whether to convert the result to a tsibble.
 #' @return A tsibble or tibble containing the MSE values, model names, series names, and forecast horizons.
+#'
 #' @examples
 #' MSE <- list(
 #'   model1 = matrix(c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6), nrow = 3),
 #'   model2 = matrix(c(0.2, 0.3, 0.4, 0.5, 0.6, 0.7), nrow = 3)
 #' )
-#' result <- transfrom_sim_MSE(MSE, to_ts = TRUE)
+#' result <- transform_sim_MSE(MSE, to_ts = TRUE)
 #'
 #' @importFrom purrr imap_dfr
 #' @importFrom dplyr mutate select row_number
 #' @importFrom tidyr pivot_longer
 #' @importFrom tsibble as_tsibble
 #' @importFrom tibble as_tibble
-#'
 #' @export
-transfrom_sim_MSE <- function(MSE, to_ts = T) {
-  df <- purrr::imap_dfr(MSE, function(mat, model_name) {
-    tibble::as_tibble(mat) %>%
-      dplyr::mutate(h = dplyr::row_number()) %>%
-      tidyr::pivot_longer(cols = -h, names_to = "series", values_to = "MSE") %>%
-      dplyr::mutate(
-        .model = model_name,
-      ) %>%
-      dplyr::select(.model, series, h, MSE)
+transform_sim_MSE <- function(MSE, to_ts = TRUE) {
+  df <- imap_dfr(MSE, function(mat, model_name) {
+    as_tibble(mat) %>%
+      mutate(h = row_number()) %>%
+      pivot_longer(-h, names_to = "series", values_to = "MSE") %>%
+      mutate(.model = model_name) %>%
+      select(.model, series, h, MSE)
   })
   if (to_ts) {
-    return(
-      tsibble::as_tsibble(df, key = c(.model, series), index = h)
-    )
+    return(as_tsibble(df, key = c(.model, series), index = h))
   }
   return(df)
 }
