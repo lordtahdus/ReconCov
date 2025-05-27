921a35dc0de5bda1180334bdf7c8ae4905878929
Vincent Su <datsu2003@gmail.com>
2025-05-27 14:40:42 +1000

    update plot heatmap function

--- R/plot.R
+++ R/plot.R
@@ -1,9 +1,29 @@
+#' Plot a heatmap of a matrix
+#'
+#' @param mat A matrix to be plotted as a heatmap.
+#' @param is_unit_spherical Logical, if TRUE, the color scale is set to [-1, 1].
+#' @return A ggplot object representing the heatmap.
+#'
+#' @examples
+#' mat <- matrix(rnorm(100), nrow = 10)
+#' plot_heatmap(mat)
+#' plot_heatmap(mat, is_unit_spherical = TRUE)
+#'
 #' @export
-plot_heatmap <- function(mat) {
+plot_heatmap <- function(mat, is_unit_spherical = FALSE) {
+  if (is_unit_spherical) {
+    limit <- c(-1, 1)
+    midpoint <- 0
+  }
+  else {
+    max_val <- max(abs(mat), na.rm = TRUE)
+    limit <- c(-max_val, max_val)
+    midpoint <- 0
+  }
   ggplot(reshape2::melt(mat), aes(Var1, Var2, fill = value)) +
     geom_tile(color = "white") +
     scale_fill_gradient2(low = "blue", high = "red", mid = "white",
-                         midpoint = 0, limit = c(-1, 1), name = "Corr") +
+                         midpoint = midpoint, limit = limit, name = "Corr") +
     theme_minimal() +
     coord_fixed()
 }
