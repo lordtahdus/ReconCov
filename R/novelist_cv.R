afeddd3cc8fe48aed1c489e6a8d31cfd4ffaa6c4
Vincent Su <datsu2003@gmail.com>
2025-08-06 14:37:33 +1000

    working on updated version novlies cv

--- R/novelist_cv.R
+++ R/novelist_cv.R
@@ -150,6 +150,120 @@ novelist_cv <- function(
 
 
 
+# novelist_cv_test <- function(
+#     y,
+#     y_hat,
+#     S,
+#     window_size,
+#     deltas = seq(0, 1, by = 0.05),
+#     h = 1,
+#     # reconcile_forecast = NULL,
+#     zero_mean = TRUE,
+#     error_metric = function(actual, fc) mean((actual - fc)^2, na.rm = TRUE),
+#     ensure_PD = TRUE,
+#     message = TRUE
+# ) {
+#   T <- nrow(y)
+#   p <- ncol(y)
+
+#   # Preliminary checks
+#   if(any(dim(y) != dim(y_hat))) {
+#     stop("Dimensions of 'y' and 'y_hat' must match.")
+#   }
+#   if(T <= window_size || window_size <= 1 || (window_size %% 1 != 0)) {
+#     stop("Window size must be integer, greater than 1 and less than nrow.")
+#   }
+
+#   # Calculate base residuals
+#   resid <- y - y_hat
+#   # Store errors
+#   cv_errors <- matrix(NA, nrow = T - window_size, ncol = length(deltas))
+
+#   # Rolling over each possible validation step
+#   # i means the "last index" of the training set is i.
+#   # Then the "validation" point is i+1 to i+h.
+#   for (i in window_size:(T - h)) {
+
+#     # Training residuals from (i-window_size+1) to i
+#     train_resid <- resid[(i - window_size + 1):i, , drop=FALSE] # drop=F keep matrix structure
+
+#     # The actual data at time i+1 to i+h
+#     actual_next <- y[(i+1) : (i+h), ]
+#     fitted_next <- y_hat[(i+1) : (i+h), ]
+
+#     # Now loop over candidate threshold delta
+#     for(d in seq_along(deltas)){
+#       delta <- deltas[d]
+#       # Estimate a NOVELIST covariance using the train_resid
+#       cov_novelist <- novelist_est(
+#         resid     = train_resid,
+#         delta     = delta,
+#         zero_mean = zero_mean,
+#         ensure_PD = ensure_PD
+#       )$cov
+
+#       if (any(eigen(cov_novelist)$values <= 1e-12)) {
+#         # stop(sort(eigen(cov_novelist)$values)[1])
+#         stop("The covariance matrix is not positive definite, cannot reconcile. Try ensure_PD = T")
+#       }
+
+#       recon_fc <- fitted_next
+#       recon_fc <- reconcile_mint(
+#         base_forecasts = fitted_next,
+#         S = S,
+#         W = cov_novelist
+#       )
+
+#       # Compute error measure for time i+1:
+#       err_val <- error_metric(actual_next, recon_fc)
+
+#       # store
+#       idx_i <- i - window_size + 1
+#       cv_errors[idx_i, d] <- err_val
+#     } # end loop over deltas
+
+#     # TODO: Refine Progress Printing
+#     if (message) {
+#       if (idx_i %% 5 == 0 || idx_i == (T - window_size)) {
+#         cat("Iteration: ", idx_i, " / ", (T - window_size), "\n")
+#       }
+#     }
+#   }
+#   cat("\n")
+
+#   # Now compute average error across all rolling iterations for each delta
+#   mean_errors <- colMeans(cv_errors, na.rm = TRUE)
+#   names(mean_errors) <- deltas
+
+#   # find best delta
+#   best_idx <- which.min(mean_errors)
+#   delta_star <- deltas[best_idx]
+
+#   # Refit final NOVELIST covariance using entire resid
+#   novelist_results <- novelist_est(
+#     resid     = resid,
+#     delta     = delta_star,
+#     zero_mean = zero_mean
+#   )
+
+#   final_cov_novelist <- novelist_results$cov
+#   lambda_star <- novelist_results$lambda
+
+#   # Check final positive definiteness
+#   if (any(eigen(final_cov_novelist)$values <= 1e-8)) {
+#     stop("The final covariance matrix is not positive definite.")
+#   }
+
+#   # Return results
+#   return(list(
+#     delta = delta_star,
+#     lambda = lambda_star,
+#     cov = final_cov_novelist,
+#     errors_by_delta = mean_errors
+#   ))
+# }
+
+
 #' Rolling Cross-Validation for NOVELIST with PCA Threshold Selection
 #' 
 #' This function performs a rolling window approach to select the threshold 'delta'.
